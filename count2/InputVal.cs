46ff63135fb78dc353eac3e695d675190aaba646
Sheng-Hui <daniel810909@gmail.com>
2021-02-23 14:56:19 +0800

    重複判斷功能，數值格式錯誤可再輸入

--- count2/InputVal.cs
+++ count2/InputVal.cs
@@ -8,11 +8,12 @@ namespace count2
     {
         public static string Getdecimal(decimal n1)
         {
-            var n2 = "";
+            //var n2 = "";
             //作為判斷數值是否為整數或分數
             double judgetDouble;
             Console.Write($"請輸入數值{n1}: ");
-            n2 = Console.ReadLine();
+            var n2 = Console.ReadLine();
+            
             if (Double.TryParse(n2, out judgetDouble))
             {
                 if ((Double.Parse(n2)) % 1 == 0)
@@ -26,10 +27,17 @@ namespace count2
             }
             else
             {
-                Console.WriteLine($"數值{ n1}: '{n2}'，無法轉換為整數或小數");
-                return null;
+                //Console.WriteLine($"數值{ n1}: '{n2}'，請輸入整數或小數");
+                //return null;
+                while (!Double.TryParse(n2, out judgetDouble))
+                {
+                    Console.WriteLine($"{n2} 格式錯誤，請輸入整數或小數");
+                    Console.Write($"\n請輸入數值{n1}: ");
+                    n2 = Console.ReadLine();
+                }
             }
             return n2;
+
         }
     }
 }
